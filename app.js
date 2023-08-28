259563d2ef75718a8086ab7ae1fea2c6bcd1ddef
qkmaxware <qkmaxware@users.noreply.github.com>
2023-08-28 02:42:11 +0000

    Deploying to gh-pages from @ qkmaxware/CaptainsLog@80c666bbbaede9fecf75d039d7049226be72e2a6 🚀

---
+++ app.js
@@ -0,0 +1,17 @@
+window.App = (function(){
+    var exports = {};
+
+    exports.DownloadFileFromStream = async (fileName, contentStreamReference) => {
+        const arrayBuffer = await contentStreamReference.arrayBuffer();
+        const blob = new Blob([arrayBuffer]);
+        const url = URL.createObjectURL(blob);
+        const anchorElement = document.createElement('a');
+        anchorElement.href = url;
+        anchorElement.download = fileName ?? '';
+        anchorElement.click();
+        anchorElement.remove();
+        URL.revokeObjectURL(url);
+    }
+
+    return exports;
+})();
