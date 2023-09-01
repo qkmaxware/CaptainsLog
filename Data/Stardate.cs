f03e845407a95e2711bfa035052c0b80bb97a962
Colin Halseth <qkmaxware@gmail.com>
2023-08-31 20:17:20 -0700

    Added stardates to log

---
+++ Data/Stardate.cs
@@ -0,0 +1,19 @@
+namespace CaptainsLog;
+
+public static class StardateExtentions {
+    // Friday, July 5, 2318
+    private static DateTime TNG0 = new DateTime(
+        2318, 07, 5, 
+        12, 00, 00, 
+        DateTimeKind.Utc
+    );
+
+    //private static double StardatesPerYear = 918.23186;
+    private static double DaysPerStardate = 0.397766856;
+
+    public static double Stardate(this DateTime date) {
+        var days = (date - TNG0).TotalDays;
+        var stardates = days / DaysPerStardate;
+        return Math.Round(stardates, 2);
+    }
+}
